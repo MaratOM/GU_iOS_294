8c3cfd0de9a27a445777c0efc6423e98cfcb90b4
Marat Mikaelyan <MMikaelyan@alfabank.ru>
2020-01-10 13:18:56 +0300

    add realm to Photos controller

--- VK/VK/Controller/PhotosController.swift
+++ VK/VK/Controller/PhotosController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import RealmSwift
 
 private let reuseIdentifier = "Cell"
 
 class PhotosController: UICollectionViewController {
 
     private var networkService = NetworkService()
+    private var realmService = RealmService()
+    private var notificationToken: NotificationToken?
+
+    private var realmObjects: Results<Photo>!
     private var photos = [Photo]()
     public var friend: Friend? = nil
     
@@ -21,20 +26,57 @@ class PhotosController: UICollectionViewController {
 
         self.collectionView.backgroundView = getBackgroundImage();
         self.title = friend?.name
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        networkService.loadPhotos(ownerId: friend!.id) { [weak self] result in
-            guard let self = self else { return }
-            
-            switch result {
-            case let .success(photos):
-                self.photos = photos as! [Photo]
-                DispatchQueue.main.async {
-                    self.collectionView.reloadData()
+        let updatedTime = try! realmService.get(Update.self).filter("dataType == %@", "photos").first?.timeStamp
+        print("photos time diff \(NSDate().timeIntervalSince1970 - Double(updatedTime ?? NSDate().timeIntervalSince1970))")
+        
+        realmObjects = try! realmService.get(Photo.self).filter("ownerId = %i", friend!.id)
+        
+        if (realmObjects.count == 0 || NSDate().timeIntervalSince1970 - Double(updatedTime!) > Update.interval) {
+            networkService.loadPhotos(ownerId: friend!.id) { result in
+                switch result {
+                case let .success(photos):
+                    print("photos got from api")
+                    
+                    try! self.realmService.save(photos)
+                    self.realmObjects = try! self.realmService.get(Photo.self)
+                    let updateTime = Update(dataType: "photos", timeStamp: NSDate().timeIntervalSince1970)
+                    try! self.realmService.save([updateTime])
+                    self.initData()
+                case let .failure(error):
+                    print(error)
                 }
-            case let .failure(error):
+            }
+        } else {
+            initData()
+        }
+        
+        self.notificationToken = realmObjects.observe({ [weak self] change in
+            guard let self = self else { return }
+            switch change {
+            case .initial:
+                break
+            case .update:
+                self.collectionView.reloadData()
+            case let .error(error):
                 print(error)
             }
-        }
+        })
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        notificationToken?.invalidate()
+    }
+    
+    private func initData() {
+        self.photos = Array(realmObjects)
+        self.collectionView.reloadData()
     }
 
     // MARK: UICollectionViewDataSource
